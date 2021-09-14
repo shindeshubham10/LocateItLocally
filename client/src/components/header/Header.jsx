028084065c32ccddf8a0f2c069caef922c34421b
AnirudhaKulkarni-prog <anirudhakulkarni771@gmail.com>
2021-09-14 23:29:21 +0530

    first commit

---
+++ client/src/components/header/Header.jsx
@@ -0,0 +1,17 @@
+
+import {AppBar , Toolbar} from '@material-ui/core'
+
+
+const Header= () => {
+
+    return(
+        <AppBar>
+            <Toolbar>
+
+            </Toolbar>
+        </AppBar>
+    );
+
+}
+
+export default Header;
