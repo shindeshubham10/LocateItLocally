b9644c3154b43c70dde6bb835fd144bcab2f0052
Yash <yash.21810545@viit.ac.in>
2021-09-18 14:48:20 +0530

    few routes added

--- client/src/App.js
+++ client/src/App.js
@@ -2,16 +2,35 @@
 import Header from './components/header/Header'
 import './App.css';
 import Home from './components/home/Home';
+import Login from './components/login/Login';
+import Contact from './components/contact/Contact';
+import About from './components/about/About';
+import Cart from './components/contact/Contact';
+import Favourites from './components/favourites/Favourites';
+
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from "react-router-dom";
 
 function App() {
 
   
   return (
     
-    <div>
+    <Router>
       <Header/>
-      <Home/>
-    </div>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/contact" component={Contact} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/cart" component={Cart} />
+        <Route exact path="/favourites" component={Favourites} />
+
+      </Switch>
+    </Router>
 
     
    
