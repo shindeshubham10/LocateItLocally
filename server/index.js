a6bf2c656f7870a98766706305d8ac2ce4945645
shubhamshinde4141 <shubham.21810417@viit.ac.in>
2021-11-12 00:10:49 +0530

    Babel Setup

--- server/index.js
+++ server/index.js
@@ -1,26 +1,30 @@
-import express from 'express'
-import cors from 'cors'     //Used for connecting to frontend
-import helmet from 'helmet' //Used for security
+require("dotenv").config();
+
+import express from 'express';
+import cors from 'cors';     //Used for connecting to frontend
+import helmet from 'helmet'; //Used for security
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 
-//import Auth from "./API/Auth/index.js" 
+import Auth from './API/Auth/index.js';
+
 const app=express();
-
-
-
-
-
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(helmet());
 app.use(cors());
-//app.use('/auth',Auth);
+app.use("/auth",Auth);
 
 
 
-dotenv.config();
+app.get("/",(req,res)=>res.json({message:"Setup Success"}))
+
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}`);
+  });
+
 mongoose
   .connect(
     `mongodb+srv://${process.env.MONGO_DB_USER}:${process.env.MONGO_DB_PASSWORD}@locate-it-locally.tunlv.mongodb.net/${process.env.MONGO_DB_DATABASE}?retryWrites=true&w=majority`,
@@ -37,10 +41,3 @@ mongoose
   });
 
 
-
-app.get("/",(req,res)=>res.json({message:"Setup Success"}))
-
-
-app.listen(process.env.PORT, () => {
-    console.log(`Server is running on port ${process.env.PORT}`);
-  });
