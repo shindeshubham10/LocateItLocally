417548ce68bfdcec21a0616e6eb6a80351afcadf
Shubham Shinde <shubham.21810417@viit.ac.in>
2021-11-25 00:08:23 +0530

    Product Detail Page Integration started

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -1,9 +1,9 @@
 import HomeSearchBar  from './SearchBar';
-import React from 'react';
+import React,{useState} from 'react';
 import { useEffect } from 'react';
 import Banner from './Banner';
 import Headings from './Headings'
-import { productDetails } from "../../constants/data";
+//import { productDetails } from "../../constants/data";
 import Cards from './Cards';
 import Footer from '../footer/footer';
 
@@ -13,23 +13,24 @@ import { useSelector, useDispatch } from 'react-redux';
 
 import { getProducts as ProductList } from '../../redux/actions/productActions';
 
+
 const Home = () => {
 
-    console.log("Inside hone 1");
+    console.log("Inside home 1");
+
+    // Getting the data from Store (Redux)
     const getProducts = useSelector(state => state.getProducts);
     console.log("Inside hone 2");
     console.log("data from database type :", typeof (getProducts));
     console.log(getProducts);
-    //const {productInfo1} = 
-    console.log("data from file type :" , typeof(productDetails));
-    console.log(productDetails);
+   
+    
     console.log("Inside hone 3");
-   // const { productInfo } = getProducts.Products;
+  
     console.log("Inside hone 4");
-    //console.log("data type of productInfo :",typeof (productInfo));
-    //console.log(productInfo);
+    
     console.log("Inside hone 5");
-    //const {productInfo} = getProducts;
+   
 
     const dispatch = useDispatch();
 
@@ -37,17 +38,18 @@ const Home = () => {
         dispatch(ProductList());
         console.log("Inside dispatch");
     }, [dispatch])
+
     return (
       <div >
              <HomeSearchBar />
              <Banner />
              <Headings name="NEW ARRIVALS"/> 
-             <Cards data={getProducts}/>
+             <Cards data={getProducts}/> 
      
               <Headings name="TOP PRODUCTS"/> 
               <MultiSlider/> 
              <Headings name="TOP SELLERS"/> 
-              <Cards data={getProducts}/>
+             <Cards data={getProducts}/> 
                    
 
         </div>
--- client/src/redux/reducers/productReducers.js
+++ client/src/redux/reducers/productReducers.js
@@ -17,4 +17,19 @@ export const getProductReducer = (state = {products:[]},action) => {
        
     
 
-}
+};
+
+export const getProductDetailsReducer = (state = {ProductDetails:{}},action) => {
+    
+    switch (action.type) {
+        
+        case actionsType.GET_PRODUCT_DETAILS_SUCCESS:
+            return { Products: action.payload };
+        
+        case actionsType.GET_PRODUCT_DETAILS_FAILURE:
+            return { Error: action.payload };
+
+        default:
+            return state;
+    }
+};
